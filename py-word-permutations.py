128c48b91358a25c223b137277a0e159fdcb1fc9
Afaan Bilal <afaanbilal@gmail.com>
2016-09-13 21:12:13 +0530

    Added comments

--- py-word-permutations.py
+++ py-word-permutations.py
@@ -11,6 +11,7 @@
 #
  
 def base_convert(number, fromBase, toBase):
+    ''' Convert numbers to and from decimal '''
     i = 0
     result = 0
     number = int(number)
@@ -21,6 +22,7 @@ def base_convert(number, fromBase, toBase):
     return str(result)
     
 def binSearch(arr, s, ubound):
+    ''' Binary Search '''
     b = 0
     t = ubound
     mid = (t + b) // 2
@@ -48,6 +50,7 @@ lcount = len(chars)
 keys = ''.zfill(lcount)
 chars = sorted(chars)
 
+# find number of unique characters
 uchars = []
 
 for c in chars[:]:
@@ -56,13 +59,18 @@ for c in chars[:]:
     uchars.append(c)
     
 ucount = len(uchars)
+
+# calculate total number of permutations
 wcount = lcount ** lcount
+
+# calculate number of unique permutaions
 real_wcount = ucount ** lcount
 
 print("Total  Characters  : {}".format(lcount))
 print("Total  Permutations: {}".format(wcount))
 print("Unique Permutations: {}".format(real_wcount))
 
+# permute the characters
 words = []
 for i in range(wcount):
     cur_word = []
@@ -71,13 +79,18 @@ for i in range(wcount):
         cur_word.append(str(chars[int(key)]))
     
     c_word = ''.join(cur_word)
+
+    # calculate the next key string
     keys = base_convert(keys, lcount, 10)
     keys_i = int(keys)
     keys_i += 1 
     keys = str(keys_i)
     keys = base_convert(keys, 10, lcount)
+    
+    # pad the key string to have indexes for all places
     keys = keys.zfill(lcount)
     
+    # add only unique permutations
     if not binSearch(words, c_word, len(words)):
         words.append(c_word)
     
